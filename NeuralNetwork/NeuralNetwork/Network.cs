225140cd98941f63770c6e8a49ca70c1fc788522
MadcowDeity <carlostnb@gmail.com>
2013-12-27 10:28:27 -0700

    Initial commit of project solution and other important base files.

---
+++ NeuralNetwork/NeuralNetwork/Network.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NeuralNetwork
+{
+    class Network
+    {
+        private int p1;
+        private int p2;
+        private int p3;
+
+        public Network(Sigmoid sigmoid, int p1, int p2, int p3)
+        {
+            // TODO: Complete member initialization
+            this.sigmoid = sigmoid;
+            this.p1 = p1;
+            this.p2 = p2;
+            this.p3 = p3;
+        }
+
+        #region Sigmoids
+        /// <summary>
+        /// The hyperbolic tangent activation function.
+        /// </summary>
+        public static Sigmoid HyperbolicTangent =
+            new Sigmoid(x => Math.Tanh(x), x => 1 - Math.Pow(Math.Tanh(x), 2));
+
+        public static Sigmoid Logistic =
+            new Sigmoid(x => 1/(1+Math.Exp(-x)), x => Math.Exp(x)/Math.Pow(1 + Math.Exp(x),2));
+
+        public static Sigmoid Linear =
+            new Sigmoid(x => x, x => 1);
+
+        #endregion Sigmoids
+    }
+}
---
+++ NeuralNetwork/NeuralNetwork/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuralNetwork
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Network nn = new Network(Network.HyperbolicTangent, 2, 2, 2);
+        }
+    }
+}
