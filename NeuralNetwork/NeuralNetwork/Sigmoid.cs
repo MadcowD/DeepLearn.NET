414dea5f537dfcbd271c779186bb31da46bb5fd8
MadcowDeity <carlostnb@gmail.com>
2013-12-27 10:36:12 -0700

    Added sigmoid activation class where each activation function is described by lamda definition of its mathematical form and the derivative thereof. Logistic, Hyperbolic Tangent, and Linear are defined in the neural network class.

---
+++ NeuralNetwork/NeuralNetwork/Sigmoid.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NeuralNetwork
+{
+    /// <summary>
+    /// Defines some generic sigmoid activation function and its derivative.
+    /// </summary>
+    public class Sigmoid
+    {
+        /// <summary>
+        /// Defines the sigmoid activation function by a function and its derivative.
+        /// </summary>
+        /// <param name="function"></param>
+        /// <param name="derivative"></param>
+        public Sigmoid(Func<double, double> function, Func<double, double> derivative)
+        {
+            this.function = function;
+            this.derivative = derivative;
+        }
+
+        #region Fields
+
+        Func<double, double> derivative;
+        Func<double, double> function;
+
+        #endregion Fields
+
+        #region Properties
+
+        /// <summary>
+        /// The derivative of this sigmoid activation function.
+        /// </summary>
+        public Func<double, double> Derivative
+        { get { return derivative; } }
+
+        /// <summary>
+        /// The definition for this sigmoid activation function.
+        /// </summary>
+        public Func<double, double> Function
+        { get { return function; } }
+
+
+        #endregion Properties
+    }
+}
