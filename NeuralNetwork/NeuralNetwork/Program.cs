7a736be33d975f9058a2564535b815aad5da2e78
William Guss <carlostnb@gmail.com>
2013-12-28 14:06:56 -0700

    Taught the network XOR.

--- NeuralNetwork/NeuralNetwork/Program.cs
+++ NeuralNetwork/NeuralNetwork/Program.cs
@@ -6,10 +6,32 @@ namespace NeuralNetwork
     {
         private static void Main(string[] args)
         {
-            Network nn = new Network(Sigmoid.HyperbolicTangent, 1, 2, 1, 2);
-            Console.ReadKey();
-            nn.FeedForward(new double[] { 1, 0 });
-            Console.ReadKey();
+            Network nn = new Network(Sigmoid.HyperbolicTangent,1.1, 2, 1, 4);
+            while (true)
+            {
+                nn.FeedForward(new double[] { 1, 0 });
+                nn.BackPropagate(new double[] { 1 });
+                nn.UpdateWeights();
+                Console.WriteLine(nn.GlobalError);
+
+                nn.FeedForward(new double[] { 0, 1 });
+                nn.BackPropagate(new double[] { 1 });
+                nn.UpdateWeights();
+                Console.WriteLine(nn.GlobalError);
+
+                nn.FeedForward(new double[] { 0, 0 });
+                nn.BackPropagate(new double[] { 0 });
+                nn.UpdateWeights();
+                Console.WriteLine(nn.GlobalError);
+
+                nn.FeedForward(new double[] { 1, 1 });
+                nn.BackPropagate(new double[] { 0 });
+                nn.UpdateWeights();
+                Console.WriteLine(nn.GlobalError + "\n");
+                Console.ReadKey();
+            }
+
+
         }
     }
 }
