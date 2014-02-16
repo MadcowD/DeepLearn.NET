877492df00bb445958fdcb39fc1c75813b677ef3
Patrick Bo Chen <patrickbochen@gmail.com>
2014-02-15 19:09:55 -0700

    Constructed XOR for working dataset.

--- NeuralNetwork/XOR/Program.cs
+++ NeuralNetwork/XOR/Program.cs
@@ -7,8 +7,13 @@ namespace XOR
     {
         private static void Main(string[] args)
         {
-            Network nn = Network.Load(@"C:\temp\weigths.txt");
-            Trainer xorTrainer = new Trainer(nn, new XORDataSet());
+            Network nn = new Network(false, 2,4,1);
+            DataSet ds = new XORDataSet();
+            ds.Load();
+            Trainer xorTrainer = new Trainer(nn, ds);
+
+            xorTrainer.Train(100000, 0.01, 0.1, 0);
+
 
             Console.ReadKey();
         }
