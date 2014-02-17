7c0f5d46fe5407502de6e1978abc2ca677660651
MadcowDeity <carlostnb@gmail.com>
2014-02-16 23:49:04 -0700

    Adding first learning rate experiment

---
+++ DATA/EXPERIMENT/NeuralNetwork/NumericalExperiment/Experiments/LearningRateExperiment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumericalExperiment.Experiments
+{
+    public class LearningRateExperiment : Experiment
+    {
+        public override string[] Run()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
