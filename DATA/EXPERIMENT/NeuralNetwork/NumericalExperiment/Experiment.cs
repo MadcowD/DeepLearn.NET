d1f060cfa627910e9665fc84e05e7722751dab86
MadcowDeity <carlostnb@gmail.com>
2014-02-18 13:12:19 -0700

    Further LRE

--- DATA/EXPERIMENT/NeuralNetwork/NumericalExperiment/Experiment.cs
+++ DATA/EXPERIMENT/NeuralNetwork/NumericalExperiment/Experiment.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace NumericalExperiment
@@ -13,16 +14,66 @@ namespace NumericalExperiment
         {
             this.trainingSet = trainingSet;
             this.testingSet = testingSet;
+            this.data = new List<string>();
         }
 
         /// <summary>
         /// Runs the experiment
         /// <returns>The results of the experiment.</returns>
         /// </summary>
-        public abstract string[] Run();
+        public virtual void Run()
+        {
+            this.Data.Clear();
+        }
 
+        /// <summary>
+        /// Runs the experiment as a thread.
+        /// </summary>
+        /// <returns></returns>
+        public sealed bool RunAsThread()
+        {
+            if (worker == null)
+            {
+                worker = new Thread(new ThreadStart(this.Run));
+                worker.Start();
+            }
+            else
+            {
+                if (worker.IsAlive)
+                    return false;
+                else
+                    worker.Start();
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Writes data to a file and then clears the data.
+        /// </summary>
+        /// <param name="fileLocation"></param>
+        public void SaveData(string fileLocation)
+        {
+            System.IO.File.WriteAllLines(fileLocation, data.ToArray());
+            data.Clear();
+        }
+
+        #region Properties
+
+        /// <summary>
+        /// The data for a given experiment.
+        /// </summary>
+        protected List<String> data;
+
+
+        /// <summary>
+        /// The datastore location
+        /// </summary>
+        public static string DATASTORE = @"..\..\..\..\..\OUTPUT\";
+
+        #endregion
 
         #region Fields
+        Thread worker;
         DataSet trainingSet;
         DataSet testingSet;
         #endregion Fields
--- DATA/EXPERIMENT/NeuralNetwork/NumericalExperiment/Experiments/LearningRateExperiment.cs
+++ DATA/EXPERIMENT/NeuralNetwork/NumericalExperiment/Experiments/LearningRateExperiment.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NeuralLibrary;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,10 +9,31 @@ namespace NumericalExperiment.Experiments
 {
     public class LearningRateExperiment : Experiment
     {
-        public LearningRateExperiment
-        public override string[] Run()
+        /// <summary>
+        /// Initializes the learning rate experiment
+        /// </summary>
+        /// <param name="training">The set on which the learning rate will train on.</param>
+        /// <param name="testing">The set of testing experiment</param>
+        public LearningRateExperiment(DataSet training, DataSet testing) : 
+            base(training, testing)
         {
-            
         }
+
+        /// <summary>
+        /// Runs the learning rate experiment.
+        /// The experiment will consist of the following steps.
+        /// 1. Train the network at a given learning rate (0-1) n=0.05 on the traning set until convergence
+        /// 2. Once converged get the net error over the testing set (which is consitent, random) add to a learning rate error dataset.
+        /// 3. Save all converged net errors for a given experiment.
+        /// </summary>
+        public override void Run()
+        {
+
+        }
+
+        #region Fields
+        string PERSIST = @"LR\";
+        List<string> testingError = new List<string>();
+        #endregion
     }
 }
