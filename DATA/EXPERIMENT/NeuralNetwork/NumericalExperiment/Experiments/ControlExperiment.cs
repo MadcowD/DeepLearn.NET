ffb4212086a1e56885e8b401d320e46a2944b9f7
MadcowDeity <carlostnb@gmail.com>
2014-02-20 12:25:52 -0700

    Creating a control experiment. Closes #16. Contributes #19.

---
+++ DATA/EXPERIMENT/NeuralNetwork/NumericalExperiment/Experiments/ControlExperiment.cs
@@ -0,0 +1,40 @@
+﻿using NeuralLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumericalExperiment.Experiments
+{
+    public class ControlExperiment : Experiment
+    {
+        public ControlExperiment(CancerData training, CancerData testing)
+            :base(training, testing)
+        {
+        }
+
+
+        public override void Run()
+        {
+            Network nn = new Network(false, NETWORK_SIZE);
+            Trainer trainer = new Trainer(nn, trainingSet);
+
+            trainer.Train(NETWORK_EPOCHS, NETWORK_ERROR, NETWORK_LEARNING_RATE, NETWORK_MOMENTUM, NETWORK_NUDGING);
+            double testingError = testingSet.Select<DataPoint, double>(
+                (x) =>
+                {
+                    nn.FeedForward(x.Input);
+                    return 0.5 * Math.Pow(nn.Output[0] - x.Desired[0], 2);
+                }).Sum();
+
+            Console.WriteLine("Testing Error: " + testingError);
+            Analyze("", trainer, nn);
+        }
+
+        public override string PERSIST
+        {
+            get { return @"CONTROL\"; }
+        }
+    }
+}
