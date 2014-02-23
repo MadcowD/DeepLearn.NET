c3eedc0c920cee45a380036d640819f4ac689b0b
Phillip Kuznetsov <superpker@gmail.com>
2014-02-23 04:01:06 -0700

    DWT through Haar's. And some random OTSU that is not configured. Haar's still needs slight modification to work, and OTSU is not functional at the moment.

--- DATA/EXPERIMENT/NeuralNetwork/Imaging/Program.cs
+++ DATA/EXPERIMENT/NeuralNetwork/Imaging/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
+using System.Drawing.Imaging;
+
 
 namespace Imaging
 {
@@ -10,6 +13,30 @@ namespace Imaging
     {
         static void Main(string[] args)
         {
+            Bitmap bmp = new Bitmap(Image.FromFile(@"C:\temp\image.png"));
+            Bitmap oot = new Bitmap(bmp.Height, bmp.Width);
+            //Otsu os = new Otsu("C:\\temp\\cancer.png");
+            double[,] map = new double[bmp.Height, bmp.Width];
+            Console.Write(bmp.GetPixel(0,0));
+            for(int x = 0; x < bmp.Height; x++)
+            {
+                for(int y = 0; y < bmp.Width; y++)
+                {
+                    map[x, y] = System.Convert.ToDouble(bmp.GetPixel(x, y).GetBrightness());
+                }
+            }
+            Console.Write(map);
+            HaarWavelet.FWT(map,2);
+            for(int x = 0; x < bmp.Height; x++)
+            {
+                for(int y = 0; y < bmp.Width; y++)
+                {
+                    int val = 255 * (int)(map[x,y] / 255);
+                    oot.SetPixel(x, y, Color.FromArgb(val,val,val));
+                }
+            }
+            oot.Save(@"C:\temp\out.png");
+            
         }
     }
 }
