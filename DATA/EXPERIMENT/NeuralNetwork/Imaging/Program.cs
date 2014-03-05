cdb455a2be7e4e0b9d341b9a7069639e9b5838a9
Patrick Chen <patrickbochen@gmail.com>
2014-03-04 22:39:40 -0700

    Images Dataset Pre-processing

--- DATA/EXPERIMENT/NeuralNetwork/Imaging/Program.cs
+++ DATA/EXPERIMENT/NeuralNetwork/Imaging/Program.cs
@@ -14,7 +14,7 @@ namespace Imaging
     {
         static void Main(string[] args)
         {
-            Bitmap bmp = new Bitmap(Image.FromFile(@"..\..\..\..\DATASET\image2.png"));
+            Bitmap bmp = new Bitmap(Image.FromFile(@"..\..\..\..\DATASET\Pictures\Normal\mdb140N.png"));
             int[,] imageC = new int[bmp.Width, bmp.Height];
             int[,] imageProcess = new int[bmp.Width, bmp.Height];
             for (int x = 0; x < bmp.Width; x++)
@@ -23,16 +23,25 @@ namespace Imaging
             
             DWT2D d = new DWT2D(imageC);
             imageProcess = d.Transform(imageC);
-            
-           
 
             for (int x = 0; x < bmp.Width; x++)
                 for (int y = 0; y < bmp.Height; y++)
                     bmp.SetPixel(x, y, Color.FromArgb(imageProcess[x, y], imageProcess[x, y], imageProcess[x, y]));
+            bmp.Save(@"..\..\..\..\DATASET\Pictures\Normal\OUTmdb140N.png");
 
-            bmp.Save(@"..\..\..\..\DATASET\output.png");
-            Console.WriteLine("done");
-            Console.ReadKey();
+            Bitmap sbmp = new Bitmap(4, 4);
+            string[] numout = new string[4 * 4];
+            for (int x = 3; x < 7; x++)
+            {
+                for (int y = 0; y < 4; y++)
+                {
+                    sbmp.SetPixel(x-3, y, Color.FromArgb(imageProcess[x, y], imageProcess[x, y], imageProcess[x, y]));
+                    numout[4 * (x - 3) + y] = imageProcess[x, y].ToString();
+                }
+            }
+            sbmp.Save(@"..\..\..\..\DATASET\Pictures\Normal\OUTSMALLmdb140N.png");
+            System.IO.File.WriteAllLines(@"..\..\..\..\DATASET\Pictures\Normal\OUTSMALLNUMmdb140N.txt", numout);
+
         }
     }
 }
