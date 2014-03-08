47727953e4630480bd3674912d3cb31ce70853a9
Patrick Chen <patrickbochen@gmail.com>
2014-03-08 14:32:57 -0700

    Reorganized images

--- DATA/EXPERIMENT/NeuralNetwork/Imaging/Program.cs
+++ DATA/EXPERIMENT/NeuralNetwork/Imaging/Program.cs
@@ -14,7 +14,7 @@ namespace Imaging
     {
         static void Main(string[] args)
         {
-            Bitmap bmp = new Bitmap(Image.FromFile(@"..\..\..\..\DATASET\Pictures\Normal\mdb140N.png"));
+            Bitmap bmp = new Bitmap(Image.FromFile(@"..\..\..\..\DATASET\Pictures\Normal\mdb140.png"));
             int[,] imageC = new int[bmp.Width, bmp.Height];
             int[,] imageProcess = new int[bmp.Width, bmp.Height];
             for (int x = 0; x < bmp.Width; x++)
@@ -27,20 +27,20 @@ namespace Imaging
             for (int x = 0; x < bmp.Width; x++)
                 for (int y = 0; y < bmp.Height; y++)
                     bmp.SetPixel(x, y, Color.FromArgb(imageProcess[x, y], imageProcess[x, y], imageProcess[x, y]));
-            bmp.Save(@"..\..\..\..\DATASET\Pictures\Normal\OUTmdb140N.png");
+            bmp.Save(@"..\..\..\..\DATASET\Pictures\Normal\OUTmdb140.png");
 
-            Bitmap sbmp = new Bitmap(4, 4);
-            string[] numout = new string[4 * 4];
-            for (int x = 3; x < 7; x++)
+            Bitmap sbmp = new Bitmap(8, 8);
+            string[] numout = new string[8 * 8];
+            for (int x = 7; x < 15; x++)
             {
-                for (int y = 0; y < 4; y++)
+                for (int y = 8; y < 16; y++)
                 {
-                    sbmp.SetPixel(x-3, y, Color.FromArgb(imageProcess[x, y], imageProcess[x, y], imageProcess[x, y]));
-                    numout[4 * (x - 3) + y] = imageProcess[x, y].ToString();
+                    sbmp.SetPixel(x-7, y-8, Color.FromArgb(imageProcess[x, y], imageProcess[x, y], imageProcess[x, y]));
+                    numout[8 * (x - 7) + y-8] = imageProcess[x, y].ToString();
                 }
             }
-            sbmp.Save(@"..\..\..\..\DATASET\Pictures\Normal\OUTSMALLmdb140N.png");
-            System.IO.File.WriteAllLines(@"..\..\..\..\DATASET\Pictures\Normal\OUTSMALLNUMmdb140N.txt", numout);
+            sbmp.Save(@"..\..\..\..\DATASET\Pictures\Normal\OUTSMALLmdb140.png");
+            System.IO.File.WriteAllLines(@"..\..\..\..\DATASET\Pictures\Normal\OUTSMALLNUMmdb140.txt", numout);
 
         }
     }
